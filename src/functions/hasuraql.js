5af4f241ff1d445e95db960170598c63e05fb368
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-14 16:44:42 +0200

    Add dotenv.

--- src/functions/hasuraql.js
+++ src/functions/hasuraql.js
@@ -1,6 +1,10 @@
+import { HASURA_ACCESS_KEY } from './config';
+
 export const handler = (event, context, callback) => {
+  console.log(context);
+
   callback(null, {
     statusCode: 200,
-    body: 'PONG',
+    body: 'TESTY',
   });
 };
--- src/lambda/ping.js
+++
@@ -1,3 +0,0 @@
-export const handler = (event, context, callback) => {
-  callback(null, { body: 'PONG!', statusCode: 200 });
-};
