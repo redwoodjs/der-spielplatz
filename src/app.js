dfcf270b74e26af2197a661ed9f11a2174bf4d7d
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-14 10:39:15 +0200

    Use UserTools.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <>Hello, world</>;
+import UserTools from 'src/components/UserTools';
+
+const App = () => (
+  <>
+    <UserTools />
+  </>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
