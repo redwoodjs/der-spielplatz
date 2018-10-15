12d55c1d3fee63d992aa8cfdda621de3b6b70f10
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-15 22:47:27 +0200

    Add testbed.

--- lambda/index.js
+++ lambda/index.js
@@ -1,18 +1,18 @@
 import { createConnection } from 'lambda/lib/database';
 
-import Post from 'lambda/entities/Post';
-import Comment from 'lambda/entities/Comment';
+// import Post from 'lambda/entities/Post';
+// import Comment from 'lambda/entities/Comment';
 
 createConnection()
   .then(async connection => {
-    const post = new Post();
-    post.title = 'this is a test';
-    post.text = ' yo yo yo sausages';
-    const comment = new Comment();
-    comment.name = 'peterp';
-    comment.comment = 'I kinda really love cheese.';
-    post.comments = [comment];
-    await connection.manager.save(post);
+    // const post = new Post();
+    // post.title = 'this is a test';
+    // post.text = ' yo yo yo sausages';
+    // const comment = new Comment();
+    // comment.name = 'peterp';
+    // comment.comment = 'I kinda really love cheese.';
+    // post.comments = [comment];
+    // await connection.manager.save(post);
   })
   .catch(e => {
     console.log('---------------------');
