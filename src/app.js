af3c99ef14c44d6312fb7995ec2c1384cff0065b
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-15 22:47:41 +0200

    what are these?

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
 
-import UserTools from 'src/components/UserTools';
+function graphQLFetcher(graphQLParams) {
+  return fetch(`${window.location.origin}:9000/graphql`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
 
-const App = () => (
-  <>
-    <UserTools />
-  </>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GraphiQL fetcher={graphQLFetcher} />, document.body);
