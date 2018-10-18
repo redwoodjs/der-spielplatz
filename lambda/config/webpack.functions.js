35c00a85afa781c29411e6ed6e057c33aa248404
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-19 01:00:05 +0200

    Use custom webpack config to help.

---
+++ lambda/config/webpack.functions.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // https://github.com/bitinn/node-fetch/issues/493#issuecomment-414111024
+  resolve: {
+    alias: {
+      'node-fetch$': 'node-fetch/lib/index.js',
+    },
+  },
+
+  // https://github.com/websockets/ws/issues/1220
+  module: {
+    noParse: /ws/,
+  },
+  externals: /ws/,
+};
