227820311848f022e1ead54e75a43dc3e0709118
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-18 23:01:45 +0200

    Try an insert.

--- lambda/functions/ping.js
+++ lambda/functions/ping.js
@@ -1,6 +1,17 @@
+import { createConnection } from 'lambda/lib/database';
+
+import Post from 'lambda/entities/Post';
+
 export const handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: 'PONG',
+  createConnection().then(async () => {
+    const post = new Post();
+    post.title = 'this is a test';
+    post.text = ' yo yo yo rambo';
+    await post.save();
+
+    callback(null, {
+      statusCode: 200,
+      body: 'PONG',
+    });
   });
 };
