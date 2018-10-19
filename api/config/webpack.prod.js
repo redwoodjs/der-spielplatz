d085dc501cbc373f913592aa1de5b988b9bc3ebf
Tom Preston-Werner <tom@mojombo.com>
2018-10-19 23:56:48 +0200

    Do not need these.

--- api/config/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const merge = require('webpack-merge');
-
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-  mode: 'production',
-});
