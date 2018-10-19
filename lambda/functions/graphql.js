1a1d67dfd6fce065c548b957689278083af975c3
Tom Preston-Werner <tom@mojombo.com>
2018-10-19 02:05:27 +0200

    Make it work with Sequelize and PG on a real db.

--- lambda/functions/graphql.js
+++ lambda/functions/graphql.js
@@ -1,6 +1,22 @@
 import { ApolloServer, gql } from 'apollo-server-lambda';
-import { Post } from 'lambda/entities/Post';
-import { createConnection } from 'lambda/lib/database';
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  'postgres://root:yzzCQ6e&3m>DDA@chainsaw.cwptmkqzxp8y.us-west-2.rds.amazonaws.com:5432/chainsaw'
+);
+
+const Post = sequelize.define('post', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  text: {
+    type: Sequelize.STRING,
+  },
+});
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -9,24 +25,10 @@ const typeDefs = gql`
   }
 `;
 
-const go = () => {
-  const val = createConnection()
-    .then(async connection => {
-      console.log(connection);
-      const post = await Post.findOne();
-      return post.title;
-    })
-    .catch(error => {
-      console.log(error);
-      return error;
-    });
-  return val;
-};
-
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: go,
+    hello: () => Post.findById(1).then(user => user.title),
   },
 };
 
