f7cc01a17e540c5c1d1dc701abae211be0cbe8b8
Peter Pistorius <peter.pistorius@gmail.com>
2018-10-30 21:31:53 +0100

    Get web up and running.

--- web/src/app.js
+++ web/src/app.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GraphiQL from 'graphiql';
-import fetch from 'isomorphic-fetch';
-
-function graphQLFetcher(graphQLParams) {
-  return fetch(`${window.location.origin}/.netlify/functions/graphql`, {
-    method: 'post',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(graphQLParams),
-  }).then(response => response.json());
-}
-
-ReactDOM.render(
-  <GraphiQL fetcher={graphQLFetcher} />,
-  document.getElementById('root')
-);
+console.log("sausage");
