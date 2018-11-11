c53831fcf153f78db5333439dad3c7681a0bea8c
Peter Pistorius <peter.pistorius@gmail.com>
2018-11-11 15:53:26 +0100

    Clean up knexfile.

--- api/knexfile.js
+++ api/knexfile.js
@@ -4,22 +4,21 @@ const {
   DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE,
 } = process.env;
 
-console.log('Using connection info:');
-console.log({
+const connection = {
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USER,
-  password: '*******',
+  password: DB_PASSWORD,
   database: DB_DATABASE,
+};
+
+console.log('Using connection info:');
+console.log({
+  ...connection,
+  password: '*****',
 });
 
 module.exports = {
   client: 'pg',
-  connection: {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-  },
+  connection,
 };
