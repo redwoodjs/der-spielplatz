a9498778e38ce04c99d21a41bf9e32c29503d885
Peter Pistorius <peter.pistorius@gmail.com>
2018-11-11 09:04:02 +0100

    Create some routes.

--- web/src/app.js
+++ web/src/app.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import client from 'src/lib/apolloGraphQLClient';
 import Main from 'src/components/Main';
 
+const Admin = () => <h2>I'm the admin son.</h2>;
+
 const App = () => (
   <ApolloProvider client={client}>
-    <Main />
+    <div>
+      <a href="/">Categories</a>
+      {' | '}
+      <a href="/admin">Admin</a>
+      {' | '}
+    </div>
+    <Router>
+      <>
+        <Route path="/" exact component={Main} />
+        <Route path="/admin" component={Admin} />
+      </>
+    </Router>
   </ApolloProvider>
 );
 
