411fea965b9f4bbb3e4f735d95ddc51c1a6a6fbf
Peter Pistorius <peter.pistorius@gmail.com>
2019-01-27 21:12:10 +0100

    Convert routes to reach.

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Router } from '@reach/router';
 
 import { client } from 'src/lib/graphql';
 import Header from 'src/components/Header';
@@ -15,17 +15,12 @@ import './global.css';
 
 const App = () => (
   <ApolloProvider client={client}>
+    <Header />
     <Router>
-      <>
-        <Header />
-
-        <Switch>
-          <Route path="/edit/:postSlug" exact component={EditPage} />
-          <Route path="/:categorySlug/:postSlug" exact component={PostPage} />
-          <Route path="/:categorySlug" exact component={CategoryPage} />
-          <Route path="/" exact component={HomePage} />
-        </Switch>
-      </>
+      <HomePage default path="/" />
+      <CategoryPage path="/:categorySlug" />
+      <PostPage path="/:categorySlug/:postSlug" />
+      <EditPage path="/edit/:postSlug" />
     </Router>
   </ApolloProvider>
 );
