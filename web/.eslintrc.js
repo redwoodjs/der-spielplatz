22e32f8f3250cb5e5eb9571606a8dea9e6473803
Peter Pistorius <peter.pistorius@gmail.com>
2019-02-17 19:50:00 +0100

    Upgrade React and friends.

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: '@peterp/eslint-config',
+  plugins: ['react-hooks'],
   rules: {
     indent: ['error', 2],
+    'react-hooks/rules-of-hooks': 'error',
   },
   env: {
     browser: true,
